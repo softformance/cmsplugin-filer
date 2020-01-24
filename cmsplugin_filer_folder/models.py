9902cf801d5af4624b2133402908b147856d9b08
Dmytro Litvinov <litvinov.dmytro.it@gmail.com>
2020-01-24 22:21:08 +0200

    Update models.py

--- cmsplugin_filer_folder/models.py
+++ cmsplugin_filer_folder/models.py
@@ -5,7 +5,10 @@ from django.db import models
 from cms.models import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
 from filer.fields.folder import FilerFolderField
-from filer.utils.compatibility import python_2_unicode_compatible
+try:
+    from filer.utils.compatibility import python_2_unicode_compatible
+except ImportError:
+    from django.utils.encoding import python_2_unicode_compatible
 from .conf import settings
 from cmsplugin_filer_utils import FilerPluginManager
 
